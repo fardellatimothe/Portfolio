696cd571c5aa7c4dbc5c8995fe18044a147a8509
fardellatimothe <timothe.fardella@viacesi.fr>
2024-10-28 01:06:48 +0100

    Continuation Portfolio
    Section Contact Finish

--- script.js
+++ script.js
@@ -3,11 +3,6 @@ function refreshPage() {
     location.replace();
 }
 
-// Validation du formulaire de contact
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Merci pour votre message !");
-});
 
 function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
@@ -20,4 +15,52 @@ window.addEventListener('scroll', () => {
     } else {
         button.classList.remove('show');
     }
+});
+
+function sendMail() {
+    var parms = {
+        name: document.getElementById("name").value,
+        mail : document.getElementById("mail").value,
+        message : document.getElementById("message").value,
+    }
+
+    emailjs.send("service_wo1zwti", "template_16lknf9", parms)
+        .then(function() {
+            alert("Merci de m'avoir contacté !!");
+            document.getElementById("name").value = "";
+            document.getElementById("mail").value = "";
+            document.getElementById("message").value = "";
+        })
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const words = [ "Bienvenue sur mon Portfolio !", "Je m'appelle Timothé !", "Étudiant aujourd'hui, Ingénieur demain.", "Développement Web",];
+    let wordIndex = 0;
+    let letterIndex = 0;
+    const speed = 120; // Vitesse de chaque lettre
+    const delay = 1000; // Pause entre les mots
+    const element = document.getElementById("welcome-text");
+
+    function typeWriter() {
+        if (letterIndex < words[wordIndex].length) {
+            element.innerHTML += words[wordIndex].charAt(letterIndex);
+            letterIndex++;
+            setTimeout(typeWriter, speed);
+        } else {
+            setTimeout(eraseText, delay); // Commence à effacer après un délai
+        }
+    }
+
+    function eraseText() {
+        if (letterIndex > 0) {
+            element.innerHTML = element.innerHTML.slice(0, -1); // supprime la derniere lettre
+            letterIndex--;
+            setTimeout(eraseText, speed / 3); // efface plus rapidement
+        } else {
+            wordIndex = (wordIndex + 1) % words.length; // mot suivant
+            setTimeout(typeWriter, speed);
+        }
+    }
+    
+    typeWriter();
 });
